3c5df84b198953de7ca288c82f28fd04ed5145ad
ravi <ravitejap@sendfast.in>
2020-05-04 18:50:11 +0530

    added navbar and login modal

--- website/baseapp/urls.py
+++ website/baseapp/urls.py
@@ -1,10 +1,10 @@
 
 from django.urls import path
-from .views import login,cart,checkout,store
+from .views import cart,checkout,store
 
 urlpatterns = [
-path('',login,name='login'),
-path('store/',store,name='store'),
+# path('',login,name='login'),
+path('',store,name='store'),
 path('cart/',cart,name='cart'),
 path('checkout/',checkout,name='checkout')
 
--- website/baseapp/views.py
+++ website/baseapp/views.py
@@ -3,8 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def login(request):
-    return render(request,"login.html")
 
 def cart(request):
     return render(request,"cart.html")
